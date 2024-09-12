4e85261d61ef21f6fe367ca5836ddbfce83c48d8
ming <choimg51984@gmail.com>
2024-09-12 10:08:18 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  console.log('test2');
+  console.log('test');
   return (
     <BrowserRouter>
       <div className="App">
