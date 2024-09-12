ad8c3745a9f48a7c416c9ffd69a112a691cbf784
ming <choimg51984@gmail.com>
2024-09-12 12:31:25 +0900

    endpoint modify

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,14 +1,14 @@
-const GET_AREAS_API_URL = "http://localhost:8000/get_areas";
-const POST_AREAS_API_URL = "http://localhost:8000/post_areas";
-const DELETE_AREAS_API_URL = "http://localhost:8000/delete_area";
+const GET_AREAS_API_URL = 'https://campingback.cmehapdls.com/get_areas';
+const POST_AREAS_API_URL = 'https://campingback.cmehapdls.com/post_areas';
+const DELETE_AREAS_API_URL = 'https://campingback.cmehapdls.com/delete_area';
 //
 
-const GET_TASKS_API_URL = "http://localhost:8000/get_tasks";
-const POST_TASKS_API_URL = "http://localhost:8000/post_tasks";
-const DELETE_TASKS_API_URL = "http://localhost:8000/delete_task";
+const GET_TASKS_API_URL = 'https://campingback.cmehapdls.com/get_tasks';
+const POST_TASKS_API_URL = 'https://campingback.cmehapdls.com/post_tasks';
+const DELETE_TASKS_API_URL = 'https://campingback.cmehapdls.com/delete_task';
 const UPDATE_COMPLETED_TASKS_URL =
-  "http://localhost:8000/update_completed_task";
-const UPDATE_TASK_API_URL = "http://localhost:8000/update_task";
+  'https://campingback.cmehapdls.com/update_completed_task';
+const UPDATE_TASK_API_URL = 'https://campingback.cmehapdls.com/update_task';
 
 export {
   GET_AREAS_API_URL,
